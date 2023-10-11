c73a851eaa3886022ccb2029a66e5a5829f9d0c6
vitormelo012 <vitor.melo@sga.pucminas.br>
2023-10-11 19:03:43 -0300

    update classe Cliente

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,42 +1,73 @@
+import java.util.ArrayList;
+
 public class Cliente {
 
-	private String nome;
-	private String id;
-	private Veiculo[] veiculos;
+    private String nome;
+    private String id;
+    private Veiculo[] veiculos;
 
-
-
-
-	public Cliente(String nome, String id) {
-		
-		this.nome = nome;
+    public Cliente(String nome, String id) {
+        this.nome = nome;
         this.id = id;
-	}
+        
+    }
 
-	public void addVeiculo(Veiculo veiculo) {
-		
-	}
+    public void addVeiculo(Veiculo veiculo) {
+        for (int i = 0; i < veiculos.length; i++) {
+            if (veiculos[i] == null) {
+                veiculos[i] = veiculo;
+               
+            }
+        }
+    }
 
-	public Veiculo possuiVeiculo(String placa) {
-		
-	}
+    public Veiculo possuiVeiculo(String placa) {
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null && veiculo.getPlaca().equals(placa)) {
+                return veiculo;
+            }
+        }
+        return null;
+    }
 
-	public int totalDeUsos() {a
-		
-	}
+    public int totalDeUsos() {
+        int totalUsos = 0;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null) {
+                totalUsos += veiculo.totalDeUsos();
+            }
+        }
+        return totalUsos;
+    }
 
-	public double arrecadadoPorVeiculo(String placa) {
-		
-	}
+    public double arrecadadoPorVeiculo(String placa) {
+        Veiculo veiculo = possuiVeiculo(placa);
+        if (veiculo != null) {
+            return veiculo.totalArrecadado();
+        }
+        return 0;
+    }
 
-	public double arrecadadoTotal() {
-		
-	}
+    public double arrecadadoTotal() {
+        double totalArrecadado = 0;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null) {
+                totalArrecadado += veiculo.totalArrecadado();
+            }
+        }
+        return totalArrecadado;
+    }
 
-	public double arrecadadoNoMes(int mes) {
-		
-	}
-	
+    public double arrecadadoNoMes(int mes) {
+        double arrecadadoNoMes = 0;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null) {
+                arrecadadoNoMes += veiculo.arrecadadoNoMes(mes);
+            }
+        }
+        return arrecadadoNoMes;
+    }
+
     // Getter para o campo nome
     public String getNome() {
         return nome;
@@ -66,5 +97,4 @@ public class Cliente {
     public void setVeiculos(Veiculo[] veiculos) {
         this.veiculos = veiculos;
     }
-
 }
