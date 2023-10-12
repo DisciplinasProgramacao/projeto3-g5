e34c41126bbb223a023e87229b6e097b2dec1944
Paulo Victor Fernandes <119937701+paulovictor0907@users.noreply.github.com>
2023-10-11 21:55:51 -0300

    Create UsoDeVaga.java

---
+++ codigo/UsoDeVaga.java
@@ -0,0 +1,67 @@
+import java.time.LocalDateTime;
+public class UsoDeVaga {
+
+	private static final double FRACAO_USO = 0.25;
+	private static final double VALOR_FRACAO = 4.0;
+	private static final double VALOR_MAXIMO = 50.0;
+	private Vaga vaga;
+	private LocalDateTime entrada;
+	private LocalDateTime saida;
+	private double valorPago;
+
+	public UsoDeVaga(Vaga vaga) {
+		this.vaga = vaga;
+		
+	}
+	//getter para vaga
+	public Vaga getVaga() {
+		return vaga;
+	}
+
+	//sett vaga
+	public void setVaga(Vaga vaga){
+		this.vaga = vaga;
+	}
+
+	//getter entrada
+	public LocalDateTime getEntrada(){
+		return entrada;
+	}
+
+	//sett entrada
+	public void setEntrada(LocalDateTime entrada){
+		this.entrada = entrada;
+	}
+
+	//get saida
+	public LocalDateTime getSaida(){
+		return saida;
+	}
+
+	//sett saida
+	public void setSaida(LocalDateTime saida){
+		this.saida = saida;
+	}
+
+	//gett valorPago
+	public double getvalorPago(){
+		return valorPago;
+	}
+
+	//set valorPago
+	public void setvalorPago(double valorPago){
+		this.valorPago = valorPago;
+	}
+	 
+
+	public double sair() {
+		return valorPago;
+		
+	}
+
+	public double valorPago() {
+		return valorPago;
+		
+	}
+
+}
