6f3991873419a1e8fba1cd9d8af31d49789d3f6e
arthur-am <arthur.mendonca.1352200@sga.pucminas.br>
2023-10-11 21:32:23 -0300

    Implementação da classe Veículo

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,33 +1,74 @@
+import java.util.*;
+
 public class Veiculo {
 
-	private String placa;
-	private UsoDeVaga[] usos;
+    private String placa;
+    private UsoDeVaga[] usos;
 
-	public Veiculo(String placa, UsoDeVaga[] usos) {
-		this.placa = placa;
-		this.usos = usos;
-	}
+    public Veiculo(String placa, int maxUsos) {
+        this.placa = placa;
+        this.usos = new UsoDeVaga[maxUsos];
+    }
 
-	public void estacionar(Vaga vaga) {
-		
-	}
+    public void estacionar(Vaga vaga) {
+        for (int i = 0; i < usos.length; i++) {
+            if (usos[i] == null) {
+				if (vaga.disponivel()) {
+					usos[i] = new UsoDeVaga(vaga, this);
+					vaga.estacionar();
+					break;
+				}
+            }
+        }
+    }
 
-	public double sair() {
-		
-	}
+    public double sair() {
+        double totalPago = 0.0;
+        for (int i = 0; i < usos.length; i++) {
+            if (usos[i] != null && !usos[i].getSaida()) {
+                usos[i].sair();
+                totalPago += usos[i].valorPago();
+                usos[i] = null;
+            }
+        }
+        return totalPago;
+    }
 
-	public double totalArrecadado() {
-		
-	}
+    public double totalArrecadado() {
+        double totalArrecadado = 0.0;
+        for (UsoDeVaga uso : usos) {
+            if (uso != null && uso.getSaida()) {
+                totalArrecadado += uso.valorPago();
+            }
+        }
+        return totalArrecadado;
+    }
 
-	public double arrecadadoNoMes(int mes) {
-		
-	}
+    public double arrecadadoNoMes(int mes) {
+        double totalArrecadadoNoMes = 0.0;
+		int mesData = data.getMonth() + 1;
+        for (UsoDeVaga uso : usos) {
+			if (uso != null) {
+				Date data = uso.getSaida();
 
-	public int totalDeUsos() {
-		
-	}
+				if (data != null && mesData == mes) {
+					totalArrecadadoNoMes += uso.valorPago();
+				}
+			}
+        }
+        return totalArrecadadoNoMes;
+    }
 
+    public int totalDeUsos() {
+        int totalUsos = 0;
+        for (UsoDeVaga uso : usos) {
+            if (uso != null) {
+                totalUsos++;
+            }
+        }
+        return totalUsos;
+    }
+	
 	public String getPlaca() {
 		return this.placa;
 	}
